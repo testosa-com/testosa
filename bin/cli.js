c21464570461e823b5edfc22ae1ae94284359f16
Howard Henry <howard.henry@testosa.com>
2021-08-31 12:55:19 -0400

    feat: enable config file initialization

--- bin/cli.js
+++ bin/cli.js
@@ -2,8 +2,14 @@
 const { hideBin } = require('yargs/helpers');
 const yargs = require('yargs/yargs');
 const testOpenApiPaths = require('../lib');
+const createInitialConfigFile = require('../lib/create-initial-config-file');
 
 const { argv } = yargs(hideBin(process.argv));
+const { init } = argv;
 const isCli = true;
 
-testOpenApiPaths(argv, isCli);
+if (init) {
+  createInitialConfigFile();
+} else {
+  testOpenApiPaths(argv, isCli);
+}
